40583234866eca29b8a04099b8df98f527254070
shekharS2 <saikiashekhar25@gmail.com>
2021-10-04 23:15:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let addTodoButton = document.querySelector(".add-todo");
+let todoInput = document.querySelector(".todo-input");
+let todosList = document.querySelector(".todos-list-container");
+
+todoInput.addEventListener("keypress", function (e) {
+    //console.log(e); //e --> event object passed to the callback function of addEventListener
+  if (e.key == "Enter") {
+    addTodo();
+  }
+});
+addTodoButton.addEventListener("click", function () {
+    //console.log(e); //e --> event object passed to the callback function of addEventListener
+  addTodo();
+});
+
+// attach click event on addTodoButton
+function addTodo() {
+  let todoInputValue = todoInput.value;
+  if (todoInputValue) {
+    appendTodo(todoInputValue);
+    // it will empty the todoInput
+    todoInput.value = "";
+  }
+}
+
+function appendTodo(todo) {
+  let todoItemDiv = document.createElement("div");
+  todoItemDiv.classList.add("todo-item");
+  // <div class="todo-item"> </div>
+
+  let pTag = document.createElement("p");
+  pTag.classList.add("todo");
+  pTag.textContent = todo;
+  // <p class="todo-input">Learn Css</p>
+
+  let deleteTodoButton = document.createElement("button");
+  deleteTodoButton.classList.add("delete-todo");
+  deleteTodoButton.textContent = "Delete";
+  // <button class="delete-todo">Delete</button>
+
+  deleteTodoButton.addEventListener("click", deleteTodo);
+
+  todoItemDiv.append(pTag);
+  todoItemDiv.append(deleteTodoButton);
+
+  todosList.append(todoItemDiv);
+}
+
+function deleteTodo(e) {
+  e.target.parentNode.remove();
+}
